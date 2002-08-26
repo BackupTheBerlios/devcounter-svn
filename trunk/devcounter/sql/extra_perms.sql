beeb71ccc38431e189307217a4be584d4e8bdb6a
helix <helix@be933cc1-49d5-0310-94f3-a026517eeeee>
2002-08-26 18:48:05 +0000

    add initial values into extra_perms

---
+++ trunk/devcounter/sql/extra_perms.sql
@@ -0,0 +1,19 @@
+# phpMyAdmin MySQL-Dump
+# version 2.2.6
+# http://phpwizard.net/phpMyAdmin/
+# http://www.phpmyadmin.net/ (download page)
+#
+# Host: localhost
+# Generation Time: Aug 26, 2002 at 08:34 PM
+# Server version: 3.23.44
+# PHP Version: 4.0.6
+# Database : `devcounter`
+
+USE devcounter;
+
+#
+# Dumping data for table `extra_perms`
+#
+
+INSERT INTO extra_perms (username, showname, showemail, search, contact) VALUES ('oldfish', 'no', 'no', 'no', 'no');
+
