ac89d5aefaf83cf142dba08866e20f9d04a4ae66
helix <helix@be933cc1-49d5-0310-94f3-a026517eeeee>
2002-08-31 14:52:55 +0000

    missing phplib prepend file added

--- trunk/devcounter/install.php
+++ trunk/devcounter/install.php
@@ -16,7 +16,7 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 or later of the GPL.
 #
-# $Id: install.php,v 1.2 2002/08/27 09:59:41 helix Exp $
+# $Id: install.php,v 1.3 2002/08/31 14:52:55 helix Exp $
 #
 ######################################################################  
 
@@ -85,6 +85,7 @@ case "check_php":
 /* Check PHPlib */
 
 case "check_phplib":
+	require("./include/prepend.php3");
 	require("./install/header.inc");
 	require("./install/check_phplib.php");
 	break;
